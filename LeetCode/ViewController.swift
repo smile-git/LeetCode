fe0ddaf2d0b01f7ce7fc09d026a62857302277b3
smile-git <zhao_wx163@163.com>
2018-09-14 11:15:25 +0800

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by ZWX on 2018/9/14.
+//  Copyright © 2018年 ZWX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
